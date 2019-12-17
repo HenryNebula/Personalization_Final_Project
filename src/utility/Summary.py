cd4efe477db70cd956a0b181ebcbe236ffa410ad
HenryNebula <c-huang2015011098@hotmail.com>
2019-12-18 07:45:50 +0800

    update summary functions

--- src/utility/Summary.py
+++ src/utility/Summary.py
@@ -20,7 +20,9 @@ class Summary:
         return latest
     
     def __update_table(self):
-        self.table = pd.read_sql("select * from MetaData", self.__engine, index_col="id", parse_dates="ts")
+        table = pd.read_sql("select * from MetaData", self.__engine, index_col="id")
+        table.ts = table.ts.astype(np.datetime64)
+        self.table = table.copy()
 
     def summarize_cv(self,
                      dataset_name,
@@ -61,3 +63,33 @@ class Summary:
         df = self.__get_latest_result(df, ["model", "hyper", "metric"])
 
         return df
+    
+    def get_optimal_params(self, dataset_name, model_name, metric):
+        test_perf = self.get_model_test_perf(dataset_name, model_name)
+        if len(test_perf) == 0:
+#             print("Results using dataset {} for {} of {} are not found in this database".format(dataset_name, metric, model_name)) 
+            return None
+        
+        ascending = True if metric in ["rmse"] else False
+        filtered = test_perf[test_perf.metric == metric].sort_values("value", ascending=ascending)
+        filtered.reset_index(drop=True, inplace=True)
+        hyper = filtered.loc[0, "hyper"]
+        value = filtered.loc[0, "value"]
+        print("Best {} of {} is found as {}".format(metric, model_name, value))
+        
+#         if "CollectiveMF" in model_name:
+#             use_item = "Item" in model_name
+#             use_user = "User" in model_name
+#             hyper["use_user_info"] = use_user
+#             hyper["use_item_info"] = use_item
+           
+        return hyper
+    
+    def get_result_for_params(self, dataset_name, model_name, hyper, metric):
+        test_perf = self.get_model_test_perf(dataset_name, model_name)
+
+        filtered = test_perf[(test_perf.model == model_name) & (test_perf["hyper"] == hyper) & (test_perf["metric"] == metric)].reset_index(drop=True)
+        value = filtered.loc[0, "value"]
+        
+        print("For this model, it has a {} of {}".format(metric, value))
+        return filtered
